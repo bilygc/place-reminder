07379be23ce1244e9dde9d29f7904b355c65e08a
bilygc <licbgomez@gmail.com>
2024-12-08 19:03:39 -0700

    reset page created signup blank page created

--- app/(auth)/reset-pwd.tsx
+++ app/(auth)/reset-pwd.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const SignUp = () => {
+const ResetPwd = () => {
   return (
     <View>
-      <Text>SignUp</Text>
+      <Text>ResetPwd</Text>
     </View>
   );
 };
 
-export default SignUp;
+export default ResetPwd;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,34 +1,34 @@
-import { useFonts } from 'expo-font';
-import { Stack, SplashScreen } from 'expo-router';
-import React, { useEffect } from 'react';
-import { Text } from 'react-native';
+import { useFonts } from "expo-font";
+import { Stack, SplashScreen } from "expo-router";
+import React, { useEffect } from "react";
+import { Text } from "react-native";
 
 // Import your global CSS file
-import '../global.css';
-import { View } from 'react-native-reanimated/lib/typescript/Animated';
+import "../global.css";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
 
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
   const [fontsLoaded, error] = useFonts({
-    'Inter-Black': require('../assets/fonts/Inter_18pt-Black.ttf'),
-    'Inter-BlackItalic': require('../assets/fonts/Inter_18pt-BlackItalic.ttf'),
-    'Inter-Bold': require('../assets/fonts/Inter_18pt-Bold.ttf'),
-    'Inter-BoldItalic': require('../assets/fonts/Inter_18pt-BoldItalic.ttf'),
-    'Inter-ExtraBold': require('../assets/fonts/Inter_18pt-ExtraBold.ttf'),
-    'Inter-ExtraBoldItalic': require('../assets/fonts/Inter_18pt-ExtraBoldItalic.ttf'),
-    'Inter-ExtraLight': require('../assets/fonts/Inter_18pt-ExtraLight.ttf'),
-    'Inter-ExtraLightItalic': require('../assets/fonts/Inter_18pt-ExtraLightItalic.ttf'),
-    'Inter-Italic': require('../assets/fonts/Inter_18pt-Italic.ttf'),
-    'Inter-Light': require('../assets/fonts/Inter_18pt-Light.ttf'),
-    'Inter-LightItalic': require('../assets/fonts/Inter_18pt-LightItalic.ttf'),
-    'Inter-Medium': require('../assets/fonts/Inter_18pt-Medium.ttf'),
-    'Inter-MediumItalic': require('../assets/fonts/Inter_18pt-MediumItalic.ttf'),
-    'Inter-Regular': require('../assets/fonts/Inter_18pt-Regular.ttf'),
-    'Inter-SemiBold': require('../assets/fonts/Inter_18pt-SemiBold.ttf'),
-    'Inter-SemiBoldItalic': require('../assets/fonts/Inter_18pt-SemiBoldItalic.ttf'),
-    'Inter-Thin': require('../assets/fonts/Inter_18pt-Thin.ttf'),
-    'default': require('../assets/fonts/Inter_18pt-Regular.ttf'),
+    "Inter-Black": require("../assets/fonts/Inter_18pt-Black.ttf"),
+    "Inter-BlackItalic": require("../assets/fonts/Inter_18pt-BlackItalic.ttf"),
+    "Inter-Bold": require("../assets/fonts/Inter_18pt-Bold.ttf"),
+    "Inter-BoldItalic": require("../assets/fonts/Inter_18pt-BoldItalic.ttf"),
+    "Inter-ExtraBold": require("../assets/fonts/Inter_18pt-ExtraBold.ttf"),
+    "Inter-ExtraBoldItalic": require("../assets/fonts/Inter_18pt-ExtraBoldItalic.ttf"),
+    "Inter-ExtraLight": require("../assets/fonts/Inter_18pt-ExtraLight.ttf"),
+    "Inter-ExtraLightItalic": require("../assets/fonts/Inter_18pt-ExtraLightItalic.ttf"),
+    "Inter-Italic": require("../assets/fonts/Inter_18pt-Italic.ttf"),
+    "Inter-Light": require("../assets/fonts/Inter_18pt-Light.ttf"),
+    "Inter-LightItalic": require("../assets/fonts/Inter_18pt-LightItalic.ttf"),
+    "Inter-Medium": require("../assets/fonts/Inter_18pt-Medium.ttf"),
+    "Inter-MediumItalic": require("../assets/fonts/Inter_18pt-MediumItalic.ttf"),
+    "Inter-Regular": require("../assets/fonts/Inter_18pt-Regular.ttf"),
+    "Inter-SemiBold": require("../assets/fonts/Inter_18pt-SemiBold.ttf"),
+    "Inter-SemiBoldItalic": require("../assets/fonts/Inter_18pt-SemiBoldItalic.ttf"),
+    "Inter-Thin": require("../assets/fonts/Inter_18pt-Thin.ttf"),
+    default: require("../assets/fonts/Inter_18pt-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -45,12 +45,12 @@ const RootLayout = () => {
 
   return (
     // <GlobalProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="(auth)" options={{ headerShown: false }} /> */}
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
-      </Stack>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+      {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
+    </Stack>
     // </GlobalProvider>
   );
 };
